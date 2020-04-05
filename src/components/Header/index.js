ddc86aa9cca1f8a974e61f27e8c1a1a22c6a0686
DiouMedeiros <diou.182@gmail.com>
2020-04-05 19:53:06 -0300

    Changed Home style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,11 +20,8 @@ export default function Header() {
     <div className="container-header">
       <div className="content-header">
         <nav>
-          {/* <p data-testid="tasks">TODAS TAREFAS: {todoList.length}</p> */}
-          <p data-testid="tasks-done">TAREFAS CONCLUÍDAS: {taskDone.length}</p>
-          <p data-testid="tasks-not-done">
-            TAREFAS NÃO CONCLUÍDAS: {taskNotDone.length}
-          </p>
+          <div>Algoritmo Genético</div>
+          <p>Problema do caixeiro viajante</p>
         </nav>
       </div>
     </div>
