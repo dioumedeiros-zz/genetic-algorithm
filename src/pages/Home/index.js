b0cf6a289425c71626b18c126d9286e8ea37e9da
DiouMedeiros <diou.182@gmail.com>
2020-04-07 00:23:16 -0300

    Created delete component anda input generation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdDeleteForever } from "react-icons/md";
 import { toast } from "react-toastify";
 
 import Header from "../../components/Header";
 import CityList from "../../components/CityList";
+import Modal from "../../components/Modal";
 import "./styles.css";
 
 const CityController = require("../../api/controller/CityController");
@@ -13,15 +14,23 @@ export default function List() {
   const [city, setCity] = useState("");
   const [selectList, setSelectList] = useState([]);
   const [selection, setSelection] = useState("A");
+  const [solution, setSolution] = useState();
+  const [open, setOpen] = useState(false);
+  const [generation, setGeneration] = useState(1000);
 
   useEffect(() => {
     loadCities();
   }, []);
 
   function handleAddCity() {
-    CityController.store(city);
-    loadCities();
-    setCity("");
+    const cities = JSON.parse(localStorage.getItem("cities"));
+    if (!cities || cities.length < 10) {
+      CityController.store(city);
+      loadCities();
+      setCity("");
+    } else {
+      toast.error("No máximo 10 cidades");
+    }
   }
 
   function loadCities() {
@@ -29,20 +38,35 @@ export default function List() {
 
     if (cities) {
       setSelectList(cities);
+      const index = cities.length - 1;
+      setSelection(cities[index].id);
+      setTimeout(() => {
+        document.querySelector(".select").selectedIndex = String(index);
+      }, 100);
     }
   }
 
-  function handleExecute() {
+  async function handleExecute() {
     const cities = JSON.parse(localStorage.getItem("cities"));
 
     if (isValid(cities)) {
       const algorithm = new GeneticAlgorithim(cities);
-      algorithm.initialize();
+      const bestRoute = await algorithm.initialize();
+      if (bestRoute) {
+        setSolution(bestRoute);
+        setOpen(true);
+      }
     }
   }
 
   function isValid(cities) {
-    cities.forEach((city) => {
+    for (const city of cities) {
+      if (!city.distance) {
+        toast.error(
+          `Distância inválida. Verifique os destinos da cidade ${city.name}`
+        );
+        return false;
+      }
       city.distance.forEach((dist) => {
         if (!dist.time) {
           toast.error(
@@ -51,8 +75,19 @@ export default function List() {
           return false;
         }
       });
-    });
+    }
     return true;
+  }
+
+  function handleDelete() {
+    const cities = JSON.parse(localStorage.getItem("cities"));
+    const citySelected = cities.find((c) => c.id === selection);
+    CityController.delete(citySelected);
+    loadCities();
+  }
+
+  function handleCloseModal() {
+    setOpen(false);
   }
 
   return (
@@ -72,22 +107,38 @@ export default function List() {
         </div>
         <div className="content">
           <div className="controls">
-            <select
-              className="select"
-              onChange={(e) => setSelection(e.target.value)}
-            >
-              {selectList.map((item) => (
-                <option key={item.id} value={item.id}>
-                  {item.name}
-                </option>
-              ))}
-            </select>
+            <div className="city-selected">
+              <select
+                className="select"
+                onChange={(e) => setSelection(e.target.value)}
+              >
+                {selectList.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+              <MdDeleteForever
+                size={28}
+                color="#81259d"
+                onClick={handleDelete}
+              />
+            </div>
+            <div className="generation">
+              <label>Gerações</label>
+              <input
+                placeholder="Gerações"
+                value={generation}
+                onChange={(e) => setGeneration(e.target.value)}
+              />
+            </div>
           </div>
           <CityList filter={selection} />
         </div>
         <button className="btn-execute" onClick={handleExecute}>
           EXECUTAR ALGORITMO
         </button>
+        <Modal open={open} data={solution} close={handleCloseModal} />
       </div>
     </>
   );
