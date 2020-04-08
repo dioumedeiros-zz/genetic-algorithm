6fbd1cccbadbf392f0e150c5ba5a261ac0ac8d03
DiouMedeiros <diou.182@gmail.com>
2020-04-07 22:25:06 -0300

    Updates

---
+++ precache-manifest.dcfcdeff95c7bf986ae2826848217f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fc30612671ecf26c4a0fe0f07e3de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23f3e0b95b9e9b87e34",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "53a0ed58a52998e27cdc",
+    "url": "/static/css/main.736da03c.chunk.css"
+  },
+  {
+    "revision": "d23f3e0b95b9e9b87e34",
+    "url": "/static/js/2.550e815b.chunk.js"
+  },
+  {
+    "revision": "339bc2301027dc6f62f3170009b3a391",
+    "url": "/static/js/2.550e815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a0ed58a52998e27cdc",
+    "url": "/static/js/main.b0b0cd1f.chunk.js"
+  },
+  {
+    "revision": "ca5586759421b5e549a7",
+    "url": "/static/js/runtime-main.0a96d3a4.js"
+  }
+]);
